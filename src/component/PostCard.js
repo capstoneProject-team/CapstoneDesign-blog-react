5b52ac5bd76c1e34655f9cfd07bdfed24439c67a
Lee Jin Ju <banggi1124@gmail.com>
2022-12-04 03:32:02 +0900

    refactor postcard & etc

--- src/component/PostCard.js
+++ src/component/PostCard.js
@@ -4,57 +4,54 @@ import moment from 'moment';
 import { Link } from 'react-router-dom';
 import { Divider, Col, Row, Typography } from 'antd';
 import noimage from '../image/noimage.png';
+import MainEmtoion from './MainEmotion.js'
 const PostCard = ({ detail }) => {
-  //상세페이지로 넘어가기 
-  const { Title } = Typography;
-  const moveToDetail = () => {
 
-  }
 
   const text = detail.content;
   const newtext = text.replace(/(<([^>]+)>)/ig, "");
   const newnewtext = newtext.replace(/&nbsp;/g, "");
+  // const emotion = detail.mainEmotion;
 
-  const emotion = detail.mainEmotion;
-
-  const [emotionIcon, setEmotionIcon] = useState("")
-
+  // const [emotionIcon, setEmotionIcon] = useState("")
+  // // const textEmotionToIcon = (emotion) => {
+  // //   if (emotion == 'happy') {
+  // //     setEmotionIcon("😄")
+  // //   }
+  // //   if (emotion == 'sad') {
+  // //     setEmotionIcon("😭")
+  // //   }
+  // //   if (emotion == 'angry') {
+  // //     setEmotionIcon("🤬")
+  // //   }
+  // //   if (emotion == 'hurt') {
+  // //     setEmotionIcon("🤕")
+  // //   }
+  // //   if (emotion == 'anxious') {
+  // //     setEmotionIcon("😨")
+  // //   }
+  // //   if (emotion == 'statrled') {
+  // //     setEmotionIcon("😳")
+  // //   }
+  // //   else {
+  // //     setEmotionIcon("🤪")
+  // //   }
+  // // }
+  // // useEffect(() => {
+  // //   textEmotionToIcon(emotion);
+  // // }, [])
   const handleImageError = (e) => {
     e.target.src = noimage;
   }
 
-  const textEmotionToIcon = (emotion) => {
-    if (emotion == 'happy') {
-      setEmotionIcon("😄")
-    }
-    if (emotion == 'sad') {
-      setEmotionIcon("😭")
-    }
-    if (emotion == 'angry') {
-      setEmotionIcon("🤬")
-    }
-    if (emotion == 'hurt') {
-      setEmotionIcon("🤕")
-    }
-    if (emotion == 'anxious') {
-      setEmotionIcon("😨")
-    }
-    if (emotion == 'statrled') {
-      setEmotionIcon("😳")
-    }
-    else {
-      setEmotionIcon("🤪")
-    }
-  }
-  useEffect(() => {
-    textEmotionToIcon(emotion);
-  }, [])
+
 
   return (
     <Link to={`/diary-detail/${detail.id}`} style={{ textDecoration: 'none' }}>
       <Container style={{ paddingLeft: "5%", paddingRight: "5%" }}>
         <div style={{display:"flex", justifyContent : "space-between"}}>
           <div style={{width:"75%"}}>
+            <MainEmtoion detail={detail}/>
             <h4>{emotionIcon} &nbsp; {detail.title}</h4>
             <br/>
             <div style={{fontSize:"10pt", color: "grey", paddingLeft:"6%", paddingRight:"6%"}}>
